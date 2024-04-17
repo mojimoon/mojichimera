ca455fb59221bd781f375390b700eda870cd3dcb
mojimoon <cryovit@outlook.com>
2024-04-17 18:37:15 +0800

    v0.1.5.2 Try Fixing ChaoticMod

--- mojichimera/src/main/java/mojichimera/augments/ChaoticMod.java
+++ mojichimera/src/main/java/mojichimera/augments/ChaoticMod.java
@@ -3,6 +3,7 @@ package mojichimera.augments;
 import CardAugments.cardmods.AbstractAugment;
 import basemod.abstracts.AbstractCardModifier;
 import com.evacipated.cardcrawl.modthespire.lib.SpireField;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -14,6 +15,7 @@ import java.util.HashMap;
 
 import java.util.UUID;
 
+@SpirePatch(clz = AbstractCard.class, method = SpirePatch.CLASS)
 public class ChaoticMod extends AbstractAugment {
     public static final String ID = mojichimera.makeID(ChaoticMod.class.getSimpleName());
     public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
@@ -21,19 +23,19 @@ public class ChaoticMod extends AbstractAugment {
     private static final float MULTIPLIER_MIN = 0.5F;
     private static final float MULTIPLIER_MAX = 2.0F;
     private static final Random rng = new Random();
-    private static final HashMap<UUID, Integer> damageMap = new HashMap<>();
-    private static final HashMap<UUID, Integer> blockMap = new HashMap<>();
-    private static final HashMap<UUID, Integer> magicMap = new HashMap<>();
+    public static final SpireField<Integer> damageMap = new SpireField<>(() -> 0);
+    public static final SpireField<Integer> blockMap = new SpireField<>(() -> 0);
+    public static final SpireField<Integer> magicMap = new SpireField<>(() -> 0);
 
     @Override
     public void onInitialApplication(AbstractCard card) {
         try {
             if (card.baseDamage > 0)
-                damageMap.put(card.uuid, AbstractDungeon.cardRandomRng.random((int)Math.ceil(card.baseDamage * MULTIPLIER_MIN), (int)Math.ceil(card.baseDamage * MULTIPLIER_MAX)));
+                damageMap.set(card, rng.random((int)Math.ceil(card.baseDamage * MULTIPLIER_MIN), (int)Math.ceil(card.baseDamage * MULTIPLIER_MAX)));
             if (card.baseBlock > 0)
-                blockMap.put(card.uuid, AbstractDungeon.cardRandomRng.random((int) Math.ceil(card.baseBlock * MULTIPLIER_MIN), (int) Math.ceil(card.baseBlock * MULTIPLIER_MAX)));
+                blockMap.set(card, rng.random((int)Math.ceil(card.baseBlock * MULTIPLIER_MIN), (int)Math.ceil(card.baseBlock * MULTIPLIER_MAX)));
             if (card.baseMagicNumber > 0)
-                magicMap.put(card.uuid, AbstractDungeon.cardRandomRng.random((int)Math.ceil(card.baseMagicNumber * MULTIPLIER_MIN), (int)Math.ceil(card.baseMagicNumber * MULTIPLIER_MAX)));
+                magicMap.set(card, rng.random((int)Math.ceil(card.baseMagicNumber * MULTIPLIER_MIN), (int)Math.ceil(card.baseMagicNumber * MULTIPLIER_MAX)));
         } catch (Exception e) {
             Random rng = new Random();
             if (card.baseDamage > 0)
@@ -47,22 +49,22 @@ public class ChaoticMod extends AbstractAugment {
 
     @Override
     public float modifyBaseDamage(float damage, DamageInfo.DamageType type, AbstractCard card, AbstractMonster target) {
-        if (damageMap.getOrDefault(card.uuid, 0) > 0)
-            return damageMap.get(card.uuid);
+        if (damageMap.get(card) > 0)
+            return damageMap.get(card);
         return damage;
     }
 
     @Override
     public float modifyBaseBlock(float block, AbstractCard card) {
-        if (blockMap.getOrDefault(card.uuid, 0) > 0)
-            return blockMap.get(card.uuid);
+        if (blockMap.get(card) > 0)
+            return blockMap.get(card);
         return block;
     }
 
     @Override
     public float modifyBaseMagic(float magic, AbstractCard card) {
-        if (magicMap.getOrDefault(card.uuid, 0) > 0)
-            return magicMap.get(card.uuid);
+        if (magicMap.get(card) > 0)
+            return magicMap.get(card);
         return magic;
     }
 
