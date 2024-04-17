5fbb96a89d8e8813a1ebc4b94999928649439223
mojimoon <cryovit@outlook.com>
2024-04-17 18:08:09 +0800

    v0.1.5.1 Try Fixing ChaoticMod

--- mojichimera/src/main/java/mojichimera/augments/ChaoticMod.java
+++ mojichimera/src/main/java/mojichimera/augments/ChaoticMod.java
@@ -2,6 +2,7 @@ package mojichimera.augments;
 
 import CardAugments.cardmods.AbstractAugment;
 import basemod.abstracts.AbstractCardModifier;
+import com.evacipated.cardcrawl.modthespire.lib.SpireField;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
@@ -9,6 +10,9 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import mojichimera.mojichimera;
 import com.megacrit.cardcrawl.random.Random;
+import java.util.HashMap;
+
+import java.util.UUID;
 
 public class ChaoticMod extends AbstractAugment {
     public static final String ID = mojichimera.makeID(ChaoticMod.class.getSimpleName());
@@ -17,45 +21,48 @@ public class ChaoticMod extends AbstractAugment {
     private static final float MULTIPLIER_MIN = 0.5F;
     private static final float MULTIPLIER_MAX = 2.0F;
     private static final Random rng = new Random();
+    private static final HashMap<UUID, Integer> damageMap = new HashMap<>();
+    private static final HashMap<UUID, Integer> blockMap = new HashMap<>();
+    private static final HashMap<UUID, Integer> magicMap = new HashMap<>();
 
     @Override
     public void onInitialApplication(AbstractCard card) {
-//        try {
-//            if (card.baseDamage > 0)
-//                newDamage = AbstractDungeon.cardRandomRng.random((int)Math.ceil(card.baseDamage * MULTIPLIER_MIN), (int)Math.ceil(card.baseDamage * MULTIPLIER_MAX));
-//            if (card.baseBlock > 0)
-//                newBlock = AbstractDungeon.cardRandomRng.random((int)Math.ceil(card.baseBlock * MULTIPLIER_MIN), (int)Math.ceil(card.baseBlock * MULTIPLIER_MAX));
-//            if (card.baseMagicNumber > 0)
-//                newMagicNumber = AbstractDungeon.cardRandomRng.random((int)Math.ceil(card.baseMagicNumber * MULTIPLIER_MIN), (int)Math.ceil(card.baseMagicNumber * MULTIPLIER_MAX));
-//        } catch (Exception e) {
-//            Random rng = new Random();
-//            if (card.baseDamage > 0)
-//                card.baseDamage = rng.random((int)Math.ceil(card.baseDamage * MULTIPLIER_MIN), (int)Math.ceil(card.baseDamage * MULTIPLIER_MAX));
-//            if (card.baseBlock > 0)
-//                card.baseBlock = rng.random((int)Math.ceil(card.baseBlock * MULTIPLIER_MIN), (int)Math.ceil(card.baseBlock * MULTIPLIER_MAX));
-//            if (card.baseMagicNumber > 0)
-//                card.baseMagicNumber = rng.random((int)Math.ceil(card.baseMagicNumber * MULTIPLIER_MIN), (int)Math.ceil(card.baseMagicNumber * MULTIPLIER_MAX));
-//        }
+        try {
+            if (card.baseDamage > 0)
+                damageMap.put(card.uuid, AbstractDungeon.cardRandomRng.random((int)Math.ceil(card.baseDamage * MULTIPLIER_MIN), (int)Math.ceil(card.baseDamage * MULTIPLIER_MAX)));
+            if (card.baseBlock > 0)
+                blockMap.put(card.uuid, AbstractDungeon.cardRandomRng.random((int) Math.ceil(card.baseBlock * MULTIPLIER_MIN), (int) Math.ceil(card.baseBlock * MULTIPLIER_MAX)));
+            if (card.baseMagicNumber > 0)
+                magicMap.put(card.uuid, AbstractDungeon.cardRandomRng.random((int)Math.ceil(card.baseMagicNumber * MULTIPLIER_MIN), (int)Math.ceil(card.baseMagicNumber * MULTIPLIER_MAX)));
+        } catch (Exception e) {
+            Random rng = new Random();
+            if (card.baseDamage > 0)
+                card.baseDamage = rng.random((int)Math.ceil(card.baseDamage * MULTIPLIER_MIN), (int)Math.ceil(card.baseDamage * MULTIPLIER_MAX));
+            if (card.baseBlock > 0)
+                card.baseBlock = rng.random((int)Math.ceil(card.baseBlock * MULTIPLIER_MIN), (int)Math.ceil(card.baseBlock * MULTIPLIER_MAX));
+            if (card.baseMagicNumber > 0)
+                card.baseMagicNumber = rng.random((int)Math.ceil(card.baseMagicNumber * MULTIPLIER_MIN), (int)Math.ceil(card.baseMagicNumber * MULTIPLIER_MAX));
+        }
     }
 
     @Override
     public float modifyBaseDamage(float damage, DamageInfo.DamageType type, AbstractCard card, AbstractMonster target) {
-        if (card.baseDamage > 0)
-            return rng.random((int)Math.ceil(card.baseDamage * MULTIPLIER_MIN), (int)Math.ceil(card.baseDamage * MULTIPLIER_MAX));
+        if (damageMap.getOrDefault(card.uuid, 0) > 0)
+            return damageMap.get(card.uuid);
         return damage;
     }
 
     @Override
     public float modifyBaseBlock(float block, AbstractCard card) {
-        if (card.baseBlock > 0)
-            return rng.random((int)Math.ceil(card.baseBlock * MULTIPLIER_MIN), (int)Math.ceil(card.baseBlock * MULTIPLIER_MAX));
+        if (blockMap.getOrDefault(card.uuid, 0) > 0)
+            return blockMap.get(card.uuid);
         return block;
     }
 
     @Override
     public float modifyBaseMagic(float magic, AbstractCard card) {
-        if (card.baseMagicNumber > 0)
-            return rng.random((int)Math.ceil(card.baseMagicNumber * MULTIPLIER_MIN), (int)Math.ceil(card.baseMagicNumber * MULTIPLIER_MAX));
+        if (magicMap.getOrDefault(card.uuid, 0) > 0)
+            return magicMap.get(card.uuid);
         return magic;
     }
 
