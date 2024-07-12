caee0ffa604684371d57625b198be0e2831bcf71
mojimoon <cryovit@outlook.com>
2024-07-12 14:12:23 +0800

    v0.6.3 Add BootingMod

---
+++ mojichimera/src/main/java/mojichimera/augments/uncommon/BootingMod.java
@@ -0,0 +1,90 @@
+package mojichimera.augments.uncommon;
+
+import CardAugments.cardmods.AbstractAugment;
+import basemod.abstracts.AbstractCardModifier;
+import basemod.helpers.CardModifierManager;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import mojichimera.augments.AugmentHelper;
+import mojichimera.mojichimera;
+
+public class BootingMod extends AbstractAugment {
+    public static final String ID = mojichimera.makeID(BootingMod.class.getSimpleName());
+    public static final String[] TEXT = CardCrawlGame.languagePack.getUIString(ID).TEXT;
+    public static final String[] CARD_TEXT = CardCrawlGame.languagePack.getUIString(ID).EXTRA_TEXT;
+    private static final float MULTIPLIER = 2.0F;
+    private static final int PERCENT = 100;
+    private boolean modMagic;
+
+    @Override
+    public void onInitialApplication(AbstractCard card) {
+        if (cardCheck(card, c -> (doesntDowngradeMagic() && c.baseMagicNumber > 0)))
+            this.modMagic = true;
+    }
+
+    @Override
+    public float modifyBaseDamage(float damage, DamageInfo.DamageType type, AbstractCard card, AbstractMonster target) {
+        if (card.baseDamage > 0)
+            return damage * MULTIPLIER;
+        return damage;
+    }
+
+    @Override
+    public float modifyBaseBlock(float block, AbstractCard card) {
+        if (card.baseBlock > 0)
+            return block * MULTIPLIER;
+        return block;
+    }
+
+    @Override
+    public float modifyBaseMagic(float magic, AbstractCard card) {
+        if (this.modMagic)
+            return magic * MULTIPLIER;
+        return magic;
+    }
+
+    @Override
+    public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
+        addToBot(new AbstractGameAction() {
+            @Override
+            public void update() {
+                CardModifierManager.removeModifiersById(card, ID, true);
+                isDone = true;
+            }
+        });
+    }
+
+    @Override
+    public boolean validCard(AbstractCard card) {
+        return AugmentHelper.isReplayable(card)
+                && AugmentHelper.hasVariable(card);
+    }
+
+    @Override
+    public String getPrefix() { return TEXT[0]; }
+
+    @Override
+    public String getSuffix() { return TEXT[1]; }
+
+    @Override
+    public String getAugmentDescription() { return TEXT[2]; }
+
+    @Override
+    public String modifyDescription(String rawDescription, AbstractCard card) {
+        return insertAfterText(rawDescription, String.format(CARD_TEXT[0], PERCENT));
+    }
+
+    @Override
+    public AbstractAugment.AugmentRarity getModRarity() { return AbstractAugment.AugmentRarity.UNCOMMON; }
+
+    @Override
+    public AbstractCardModifier makeCopy() { return (AbstractCardModifier) new BootingMod();}
+
+    @Override
+    public String identifier(AbstractCard card) { return ID; }
+}
